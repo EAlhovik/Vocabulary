6d4fe8463e5b4b74381493dff15a0a2d98302340
Ealhovik <e.alhovik@gmail.com>
2013-04-27 10:45:03 +0300

    add vocabulary hub

---
+++ Vocabulary/Hubs/VocabularyHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SignalR.Hubs;
+
+namespace Vocabulary.Hubs
+{
+    public class VocabularyHub : Hub
+    {
+        public void SearchWords(string term)
+        {
+            for (var i = 0; i < 100;i++ )
+            {
+                System.Threading.Thread.Sleep(1);
+                Clients.PublishWords(String.Format("{0}-{0}-{1}", term,i));
+            }
+        }
+    }
+}
